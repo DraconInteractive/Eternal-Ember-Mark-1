24b2dd53008c232510d5165d0f73f1bd9010ee04
RarCeth <rarceth@gmail.com>
2016-12-10 20:53:37 +0800

    Residual Commit

---
+++ Assets/ConsoleCommand.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsoleCommand : MonoBehaviour {
+	public static ConsoleCommand consoleCommand;
+	// Use this for initialization
+	void Start () {
+		consoleCommand = GetComponent<ConsoleCommand> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.BackQuote)) {
+			
+		}
+	}
+
+	void ToggleCC () {
+		
+	}
+}
