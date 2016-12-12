526123b6c1d132cf36ec630f310ac36a8f631b37
RarCeth <rarceth@gmail.com>
2016-12-12 18:37:11 +0800

    ConsoleImplementation

--- Assets/ConsoleCommand.cs
+++ Assets/ConsoleCommand.cs
@@ -1,21 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ConsoleCommand : MonoBehaviour {
-	public static ConsoleCommand consoleCommand;
+	public InputField commandConsole;
 	// Use this for initialization
 	void Start () {
-		consoleCommand = GetComponent<ConsoleCommand> ();
+		ToggleCC (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.BackQuote)) {
-			
+			ToggleCC (!commandConsole.gameObject.activeSelf);
 		}
 	}
 
-	void ToggleCC () {
-		
+	void ToggleCC (bool state) {
+		commandConsole.gameObject.SetActive (state);
+	}
+
+	public void EnterCommand (string command) {
+		switch (command)
+		{
+		case "Test":
+			print ("Command Accepted");
+			break;
+		}
+		ToggleCC (false);
 	}
 }
