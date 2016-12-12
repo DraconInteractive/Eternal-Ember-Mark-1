91d0f7ce471566fa1d5fe9f2470dafc6a9e2936b
RarCeth <rarceth@gmail.com>
2016-12-12 22:02:11 +0800

    Animation Import

--- Assets/Camera_Script.cs
+++ Assets/Camera_Script.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 
 public class Camera_Script : MonoBehaviour {
+	Player_Script player;
+
+	GameObject target;
+
+	public float playerXO, playerYO, playerZO;
 
 	// Use this for initialization
 	void Start () {
-	
+		player = Player_Script.player;
+		SetTarget (player.gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		CameraMovement ();
+	}
+
+	void CameraMovement () {
+		Vector3 targetPosition = target.transform.position - target.transform.forward * playerZO + target.transform.up * playerYO + target.transform.right * playerXO;
+		transform.position = Vector3.MoveTowards (transform.position, targetPosition, 0.1f);
+		transform.LookAt (target.transform.position + target.transform.forward * 1 + target.transform.up * 1);
+	}
+
+	void SetTarget(GameObject go) {
+		target = go;
 	}
 }
