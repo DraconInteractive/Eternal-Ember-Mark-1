c46a73104e3e19972bc1a7eb694115031a6a26e3
RarCeth <rarceth@gmail.com>
2016-12-14 11:53:58 +0800

    Dragon Rider Demo

---
+++ Assets/SFBayStudios/SFB Dragon Rider Demo/Scripts/dragonMount.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var humanModel			: GameObject;
+var dragonRiderModel	: GameObject;
+var demoCamera			: GameObject;
+var demoCanvas			: GameObject;
+
+var dragonAnimator		: Animator;
+
+function OnTriggerEnter(other : Collider){
+	if (other.tag == "Player")
+	{
+		humanModel.SetActive(false);
+		dragonRiderModel.SetActive(true);
+		demoCamera.SetActive(true);
+		demoCanvas.SetActive(true);
+		dragonAnimator.SetTrigger("mount");
+	}
+}
---
+++ Assets/SFBayStudios/SFB Dragon Rider Demo/Scripts/passNavMeshToAnimator.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+var navMeshAgent		: NavMeshAgent;						// Agent for this character
+var charAnimator		: Animator;							// Animator for this character
+var moveOffset			: float				= 1.0;			// Offset for movement value
+
+function Update () {
+	charAnimator.SetFloat("locomotion", (Vector3.Distance(navMeshAgent.velocity, Vector3(0,0,0))) + moveOffset);		// Pass the value to the animator
+}
+
+function SetNavMeshDestination(newTransform : Transform){
+	navMeshAgent.destination	= newTransform.position;
+}
